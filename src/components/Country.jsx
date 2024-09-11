3a8274caf575978b2244cf94f54668e6d38e932c
SadikRa <sadikrahman494@gmail.com>
2024-09-11 22:19:12 +0600

    condation added done

--- src/components/Country.jsx
+++ src/components/Country.jsx
@@ -1,13 +1,20 @@
 /* eslint-disable react/prop-types */
 
+import { useState } from "react";
+
 const Country = ({ country }) => {
+    const [visited, setVisited] = useState(false)
+
+    const handleVisited = () =>  {
+        setVisited(!visited)
+    }
   console.log(country);
   return (
     <div>
       <div className="hero bg-base-200 rounded-lg">
         <div className="hero-content flex-col lg:flex-row">
           <img src={country.flags.png} className="w-80 h-60 rounded-lg shadow-2xl" />
-          <div className="w-80 h-60">
+          <div className="w-80 ">
             <h1 className="text-3xl font-bold">{country.name.common}</h1>
             <p className="py-6">
             {country.region}
@@ -15,7 +22,9 @@ const Country = ({ country }) => {
             <p className="py-6">
             {country.population}
             </p>
-            <button className="btn btn-primary">View details</button>
+            <button className="btn btn-primary" onClick={handleVisited}>View</button>
+            <br />
+            {visited ? 'i already visited the country ' : 'i want to visit'}
           </div>
         </div>
       </div>
